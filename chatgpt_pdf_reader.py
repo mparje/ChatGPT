28b65d6950d0e39f4e2d66ee65f87ebb1dd0d3b5
mparje <126131806+mparje@users.noreply.github.com>
2023-04-04 14:43:38 -0600

    Update chatgpt_pdf_reader.py

--- chatgpt_pdf_reader.py
+++ chatgpt_pdf_reader.py
@@ -1,6 +1,7 @@
 import PyPDF2
 import openai
 import streamlit as st
+import streamlit_session_state as session_state
 
 # Solicitar al usuario su clave de API
 st.title("Generador de respuestas con ChatGPT")
@@ -38,6 +39,12 @@ def generate_answer(question, text):
 
 # Definir una función para manejar la carga de archivos y la generación de respuestas
 def handle_file_upload():
+    if not session_state.get('initialized', False):
+        session_state.answer = {}
+        session_state.ratings = {}
+        session_state.custom_question_rating = None
+        session_state.initialized = True
+
     file = st.file_uploader("Sube un archivo PDF (máximo 2 MB)", type=["pdf"], accept_multiple_files=False)
     if file is not None:
         if file.size <= 2 * 1024 * 1024:
@@ -52,49 +59,46 @@ def handle_file_upload():
                 "¿Qué consecuencias derivadas de la aceptación de su tesis plantea el autor?",
             ]
 
-            ratings = {question: None for question in suggested_questions}
-            custom_question_rating = None
-
             for question in suggested_questions:
                 if st.button(question):
-                    answer = generate_answer(question, text)
-                    st.write(answer)
-                    if ratings[question] is None:
-                        rating = st.selectbox(f"Califica la respuesta del 1 al 5 para la pregunta '{question}':", [1, 2, 3, 4, 5])
-                        ratings[question] = rating
+                    if question not in session_state.answer:
+                        session_state.answer[question] = generate_answer(question, text)
+
+                    st.write(session_state.answer[question])
+
+                    if question not in session_state.ratings:
+                        session_state.ratings[question] = st.selectbox(f"Califica la respuesta del 1 al 5 para la pregunta '{question}':", [1, 2, 3, 4, 5])
                     else:
-                        rating = st.selectbox(f"Califica la respuesta del 1 al 5 para la pregunta '{question}':", [1, 2, 3, 4, 5], index=(ratings[question] - 1))
-                        ratings[question] = rating
-                    st.write(f"Has calificado la respuesta con un {rating}.")
+                        session_state.ratings[question] = st.selectbox(f"Califica la respuesta del 1 al 5 para la pregunta ‘{question}’:“, [1, 2, 3, 4, 5], index=(session_state.ratings[question] - 1))
+st.write(f"Has calificado la respuesta con un {session_state.ratings[question]}.”)
 
-            custom_question = st.text_input("O ingresa tu propia pregunta:")
-            if st.button("Enviar pregunta personalizada"):
-                answer = generate_answer(custom_question, text)
-                st.write(answer)
-                if custom_question_rating is None:
-                    custom_question_rating =                    st.selectbox("Califica la respuesta del 1 al 5:", [1, 2, 3, 4, 5])
-                else:
-                    custom_question_rating = st.selectbox("Califica la respuesta del 1 al 5:", [1, 2, 3, 4, 5], index=(custom_question_rating - 1))
-                st.write(f"Has calificado la respuesta con un {custom_question_rating}.")
+        custom_question = st.text_input("O ingresa tu propia pregunta:")
+        if st.button("Enviar pregunta personalizada"):
+            answer = generate_answer(custom_question, text)
+            st.write(answer)
+            if session_state.custom_question_rating is None:
+                session_state.custom_question_rating = st.selectbox("Califica la respuesta del 1 al 5:", [1, 2, 3, 4, 5])
+            else:
+                session_state.custom_question_rating = st.selectbox("Califica la respuesta del 1 al 5:", [1, 2, 3, 4, 5], index=(session_state.custom_question_rating - 1))
+            st.write(f"Has calificado la respuesta con un {session_state.custom_question_rating}.")
 
-            if st.button("Mostrar puntuación total"):
-                total_score = sum(rating for rating in ratings.values() if rating is not None)
-                question_count = len([rating for rating in ratings.values() if rating is not None])
-                if custom_question_rating is not None:
-                    total_score += custom_question_rating
-                    question_count += 1
+        if st.button("Mostrar puntuación total"):
+            total_score = sum(session_state.ratings.values())
+            question_count = len(session_state.ratings)
+            if session_state.custom_question_rating is not None:
+                total_score += session_state.custom_question_rating
+                question_count += 1
 
-                if question_count > 0:
-                    total_score_percentage = (total_score / (question_count * 5)) * 100
-                    st.write(f"Tu puntuación total es {total_score} de {question_count * 5} puntos, lo que equivale a {total_score_percentage:.2f}%.")
-                else:
-                    st.write("No has calificado ninguna respuesta. Por favor, haz algunas preguntas y califica las respuestas para ver la puntuación total.")
-        else:
-            st.error("El archivo supera el límite de tamaño permitido de 2 MB. Por favor, sube un archivo más pequeño.")
-
-# Definir una función principal para ejecutar el programa
+            if question_count > 0:
+                total_score_percentage = (total_score / (question_count * 5)) * 100
+                st.write(f"Tu puntuación total es {total_score} de {question_count * 5} puntos, lo que equivale a {total_score_percentage:.2f}%.")
+            else:
+                st.write("No has calificado ninguna respuesta. Por favor, haz algunas preguntas y califica las respuestas para ver la puntuación total.")
+    else:
+        st.error("El archivo supera el límite de tamaño permitido de 2 MB. Por favor, sube un archivo más pequeño.")
+Definir una función principal para ejecutar el programa
 def main():
-    handle_file_upload()
+handle_file_upload()
 
-if __name__ == "__main__":
-    main()
+if name == “main”:
+main()
