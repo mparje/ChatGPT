f4b630295eb5ffb34f164bc763e2fa4a7487b308
mparje <126131806+mparje@users.noreply.github.com>
2023-04-04 20:11:43 -0600

    Update chatgpt_pdf_reader.py

--- chatgpt_pdf_reader.py
+++ chatgpt_pdf_reader.py
@@ -1,53 +1,37 @@
-import PyPDF2
-import openai
 import streamlit as st
+import PyPDF2
+import io
 
-# Solicitar al usuario su clave de API
-st.title("Generador de respuestas con ChatGPT")
-api_key = st.text_input("Ingresa tu clave de API de OpenAI:", type="password")
+# Para mantener el estado de la sesión en Streamlit
+class _SessionState:
+    def __init__(self, **kwargs):
+        for key, val in kwargs.items():
+            setattr(self, key, val)
 
-# Configurar la API de OpenAI con la clave de API proporcionada
-openai.api_key = api_key
+def get_session_state(**kwargs):
+    session_id = str(hash(st.session_state))
+    session_states = st.session_state.setdefault("session_states", {})
+    if session_id not in session_states:
+        session_states[session_id] = _SessionState(**kwargs)
+    return session_states[session_id]
 
-# Definir una función para extraer texto de un archivo PDF
-def extract_text_from_pdf(file):
-    pdf_reader = PyPDF2.PdfReader(file)
-    num_pages = len(pdf_reader.pages)
+def extract_text_from_pdf(pdf_file):
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
     text = ""
-    for page_num in range(num_pages):
-        page = pdf_reader.pages[page_num]
-        text += page.extract_text()
+    for page_num in range(pdf_reader.numPages):
+        text += pdf_reader.getPage(page_num).extractText()
     return text
 
-# Definir una función para generar respuestas a preguntas de usuario utilizando la API de ChatGPT
 def generate_answer(question, text):
-    max_context_length = 4096 - len(question) - 30
-    truncated_text = text[:max_context_length]
+    return f"Esta es una respuesta de ejemplo para la pregunta '{question}'."
 
-    prompt = f"{truncated_text}\n\nQuestion: {question}\nAnswer:"
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-    answer = response.choices[0].text.strip()
-    return answer
-
-# Definir una función para manejar la carga de archivos y la generación de respuestas
 def handle_file_upload():
-    if not session_state.get('initialized', False):
-        session_state.answer = {}
-        session_state.ratings = {}
-        session_state.custom_question_rating = None
-        session_state.initialized = True
-
+    session_state = get_session_state(answer={})
+    
     file = st.file_uploader("Sube un archivo PDF (máximo 2 MB)", type=["pdf"], accept_multiple_files=False)
     if file is not None:
         if file.size <= 2 * 1024 * 1024:
-            text = extract_text_from_pdf(file)
+            text = extract_text_from_pdf(io.BytesIO(file.read()))
             st.write("Preguntas sugeridas:")
 
             suggested_questions = [
@@ -65,39 +49,10 @@ def handle_file_upload():
 
                     st.write(session_state.answer[question])
 
-                    if question not in session_state.ratings:
-                        session_state.ratings[question] = st.selectbox(f"Califica la respuesta del 1 al 5 para la pregunta '{question}':", [1, 2, 3, 4, 5])
-                    else:
-                        session_state.ratings[question] = st.selectbox(f"Califica la respuesta del 1 al 5 para la pregunta '{question}':", [1, 2, 3, 4, 5], index=(session_state.ratings[question] - 1))
-st.write(f"Has calificado la respuesta con un {session_state.ratings[question]}.")
+            custom_question = st.text_input("O ingresa tu propia pregunta:")
+            if st.button("Enviar pregunta personalizada"):
+                answer = generate_answer(custom_question, text)
+                st.write(answer)
 
-        custom_question = st.text_input("O ingresa tu propia pregunta:")
-        if st.button("Enviar pregunta personalizada"):
-            answer = generate_answer(custom_question, text)
-            st.write(answer)
-            if session_state.custom_question_rating is None:
-                session_state.custom_question_rating = st.selectbox("Califica la respuesta del 1 al 5:", [1, 2, 3, 4, 5])
-            else:
-                session_state.custom_question_rating = st.selectbox("Califica la respuesta del 1 al 5:", [1, 2, 3, 4, 5], index=(session_state.custom_question_rating - 1))
-            st.write(f"Has calificado la respuesta con un {session_state.custom_question_rating}.")
-
-        if st.button("Mostrar puntuación total"):
-            total_score = sum(session_state.ratings.values())
-            question_count = len(session_state.ratings)
-            if session_state.custom_question_rating is not None:
-                total_score += session_state.custom_question_rating
-                question_count += 1
-
-            if question_count > 0:
-                total_score_percentage = (total_score / (question_count * 5)) * 100
-                st.write(f"Tu puntuación total es {total_score} de {question_count * 5} puntos, lo que equivale a {total_score_percentage:.2f}%.")
-            else:
-                st.write("No has calificado ninguna respuesta. Por favor, haz algunas preguntas y califica las respuestas para ver la puntuación total.")
-    else:
-        st.error("El archivo supera el límite de tamaño permitido de 2 MB. Por favor, sube un archivo más pequeño.")
-Definir una función principal para ejecutar el programa
-def main():
-handle_file_upload()
-
-if name == "main":
-main() 
+if __name__ == "__main__":
+    handle_file_upload()
