4c80316c4b21000d05bd5f2f2e875af8bfd50bc0
mparje <126131806+mparje@users.noreply.github.com>
2023-04-04 23:15:33 -0600

    Update math.py

--- math.py
+++ math.py
@@ -11,7 +11,6 @@ left_column.title("Evaluador de Problemas de Matemáticas")
 api_key = left_column.text_input("Ingrese su clave API de OpenAI:", type="password")
 openai.api_key = api_key
 
-
 def extract_text_from_pdf(pdf_file):
     with pdfplumber.open(pdf_file) as pdf:
         text = ""
@@ -31,19 +30,22 @@ def generate_feedback(problem, solution):
     )
     return response.choices[0].text.strip()
 
-st.title("Evaluador de Problemas de Matemáticas")
-
 def handle_file_upload():
     problem = st.text_input("Ingrese el problema matemático:")
     
+    st.title("Evaluador de Problemas de Matemáticas")
 
     uploaded_file = st.file_uploader("Sube un archivo PDF con la solución propuesta (máximo 2 MB)", type=["pdf"])
     if uploaded_file is not None:
         if uploaded_file.size <= 2 * 1024 * 1024:
             solution = extract_text_from_pdf(io.BytesIO(uploaded_file.read()))
-            if st.button("Calificar solución"):
-                feedback = generate_feedback(problem, solution)
-                st.write(feedback)
+
+            if solution.strip():
+                if st.button("Calificar solución"):
+                    feedback = generate_feedback(problem, solution)
+                    st.write(feedback)
+            else:
+                st.warning("El PDF subido no contiene ninguna respuesta a un problema matemático. Por favor, sube un archivo con una solución.")
         else:
             st.error("El archivo supera el límite de tamaño permitido de 2 MB. Por favor, sube un archivo más pequeño.")
 
