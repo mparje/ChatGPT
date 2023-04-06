1a76bc740f06bb39efd7189761ff675329769a77
mparje <126131806+mparje@users.noreply.github.com>
2023-04-06 10:42:33 -0600

    Update gpt-4.py

--- gpt-4.py
+++ gpt-4.py
@@ -4,7 +4,6 @@ from streamlit_chat import message
 import pdfplumber
 import io
 
-
 # Create a left column in the Streamlit interface
 left_column = st.sidebar
 
@@ -22,43 +21,11 @@ if 'messages' not in st.session_state:
     st.session_state['messages'] = [
         {"role": "system", "content": "You are a helpful assistant."}
     ]
-if 'model_name' not in st.session_state:
-    st.session_state['model_name'] = []
-if 'cost' not in st.session_state:
-    st.session_state['cost'] = []
-if 'total_tokens' not in st.session_state:
-    st.session_state['total_tokens'] = []
-if 'total_cost' not in st.session_state:
-    st.session_state['total_cost'] = 0.0
 
-# Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
-st.sidebar.title("Sidebar")
-model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
-counter_placeholder = st.sidebar.empty()
-counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
-clear_button = st.sidebar.button("Clear Conversation", key="clear")
+# Set the model to GPT-4
+model = "gpt-4"
 
-# Map model names to OpenAI model IDs
-if model_name == "GPT-3.5":
-    model = "gpt-3.5-turbo"
-else:
-    model = "gpt-4"
-
-# reset everything
-if clear_button:
-    st.session_state['generated'] = []
-    st.session_state['past'] = []
-    st.session_state['messages'] = [
-        {"role": "system", "content": "You are a helpful assistant."}
-    ]
-    st.session_state['number_tokens'] = []
-    st.session_state['model_name'] = []
-    st.session_state['cost'] = []
-    st.session_state['total_cost'] = 0.0
-    st.session_state['total_tokens'] = []
-    counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
-
-# generate a response
+# Generate a response
 def generate_response(prompt, text=None):
     if text:
         messages = [
@@ -72,16 +39,12 @@ def generate_response(prompt, text=None):
         ]
 
     completion = openai.ChatCompletion.create(
-    model=model,
-    messages=st.session_state['messages']
-)
+        model=model,
+        messages=messages
+    )
     response = completion.choices[0].message.content
     st.session_state['messages'].append({"role": "assistant", "content": response})
-
-    total_tokens = completion.usage.total_tokens
-    prompt_tokens = completion.usage.prompt_tokens
-    completion_tokens = completion.usage.completion_tokens
-    return response, total_tokens, prompt_tokens, completion_tokens
+    return response
 
 def extract_text_from_pdf(pdf_file):
     with pdfplumber.open(pdf_file) as pdf:
@@ -103,7 +66,7 @@ def handle_file_upload():
 
             custom_question = st.text_input("Enter your question:")
             if st.button("Submit question"):
-                answer, total_tokens, prompt_tokens, completion_tokens = generate_response(custom_question, text)
+                answer = generate_response(custom_question, text)
                 st.write(answer)
 
 if __name__ == "__main__":
