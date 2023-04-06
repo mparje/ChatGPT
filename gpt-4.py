e63b5d08f33c4c8c9bcc869eb56697ea43e3653a
mparje <126131806+mparje@users.noreply.github.com>
2023-04-06 10:54:25 -0600

    Update gpt-4.py

--- gpt-4.py
+++ gpt-4.py
@@ -23,7 +23,7 @@ if 'messages' not in st.session_state:
     ]
 
 # Set the model to GPT-4
-model = "gpt-4-32k-0314"
+model = "gpt-4"
 
 # Generate a response
 def generate_response(prompt, text=None):
