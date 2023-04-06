ee9a9ab8171859996e87766c3dc049cc78aedef6
mparje <126131806+mparje@users.noreply.github.com>
2023-04-06 11:53:36 -0600

    Update gpt-4.py

--- gpt-4.py
+++ gpt-4.py
@@ -19,47 +19,67 @@ def extract_text_from_docx(docx_file):
     return text
 
 def generate_response(prompt, text=None):
-    messages = [
-        {"role": "system", "content": "Assistant is a large language model trained by OpenAI."},
-    ]
     if text:
-        messages.append({"role": "user", "content": f"Text: {text}\nQuestion: {prompt}"})
+        messages = [
+            {"role": "system", "content": "Assistant is a large language model trained by OpenAI."},
+            {"role": "user", "content": f"Text: {text}\nQuestion: {prompt}"}
+        ]
     else:
-        messages.append({"role": "user", "content": prompt})
+        messages = [
+            {"role": "system", "content": "Assistant is a large language model trained by OpenAI."},
+            {"role": "user", "content": prompt}
+        ]
 
-    completion = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=messages,
+    completion = openai.Completion.create(
+        engine="gpt-4",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.5,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.3,
+        echo=False
     )
-    response = completion.choices[0].message.content
-    return response.strip()
+    
+    response = completion.choices[0].text.strip()
+    return response
 
-def handle_file_upload(api_key):
-    if not api_key:
-        st.warning("Please enter your OpenAI API key in the sidebar.")
-        return
-
-    openai.api_key = api_key
+def handle_file_upload():
+    if "answer" not in st.session_state:
+        st.session_state["answer"] = {}
 
     st.title("Ask my Documents")
-
+    
     left_column = st.sidebar
-    left_column.title("Instructions")
-    left_column.write("This tool allows you to ask questions about the contents of your PDF or DOCX documents. Upload a document with a maximum of 4000 words or approximately 8 pages. Documents are removed from the server after 10 minutes of inactivity.")
+    left_column.write("This tool uses the GPT-4 model from OpenAI to answer questions based on uploaded documents. The documents can be in PDF or Docx format and should not exceed 4000 words or approximately 8 pages. The documents will be deleted from the server after 10 minutes of inactivity.")
+    left_column.write("Note: You must have an API key corresponding to an account authorized to use GPT-4.")
+    api_key = left_column.text_input("Enter your OpenAI API key:", type="password")
+    if api_key == "":
+        st.warning("Please enter a valid OpenAI API key.")
+        return
+    
+    openai.api_key = api_key
 
-    file = st.file_uploader("Upload a PDF or DOCX file (max 2 MB)", type=["pdf", "docx"], accept_multiple_files=False)
+    file = st.file_uploader("Upload a PDF or Docx file (max 2 MB)", type=["pdf", "docx"], accept_multiple_files=False)
     if file is not None:
         if file.size <= 2 * 1024 * 1024:
             if file.type == "application/pdf":
                 text = extract_text_from_pdf(io.BytesIO(file.read()))
             elif file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
                 text = extract_text_from_docx(io.BytesIO(file.read()))
+            
+            if len(text.split()) > 4000:
+                st.warning("The document exceeds the maximum word count (4000 words). Please upload a smaller document.")
+                return
 
             custom_question = st.text_input("Enter your question:")
             if st.button("Submit question"):
                 answer = generate_response(custom_question, text)
                 st.write(answer)
+        else:
+            st.warning("The file size exceeds the maximum limit. Please upload a smaller file.")
 
 if __name__ == "__main__":
-    api_key = st.sidebar.text_input("Enter your OpenAI API key:", type="password")
-    handle_file_upload(api_key)
+    handle_file_upload()
