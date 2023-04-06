c0e72faa252c2c30c5d40592602133196f3d2ca3
mparje <126131806+mparje@users.noreply.github.com>
2023-04-06 07:29:14 -0600

    Update gpt-4.py

--- gpt-4.py
+++ gpt-4.py
@@ -19,7 +19,7 @@ def extract_text_from_pdf(pdf_file):
     return text
 
 def generate_answer(question, text):
-    response = openai.ChatCompletion.create(
+    response = openai.Chat.Completion(
         engine="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "Assistant is a large language model trained by OpenAI."},
